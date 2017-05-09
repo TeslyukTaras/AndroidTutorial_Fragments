7e9615a7b223277c4731bdb9e16b0d5b0963e846
TeslyukTaras <taras.teslyuk@gmail.com>
2017-05-09 17:07:56 +0300

    add: initial source code and project configurations;

---
+++ app/src/main/java/com/teslyuk/androidtutorial/fragmentexample/MainActivity.java
@@ -0,0 +1,66 @@
+package com.teslyuk.androidtutorial.fragmentexample;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.teslyuk.androidtutorial.fragmentexample.fragments.AboutFragment;
+import com.teslyuk.androidtutorial.fragmentexample.fragments.ForgotPasswordFragment;
+import com.teslyuk.androidtutorial.fragmentexample.fragments.SignInFragment;
+import com.teslyuk.androidtutorial.fragmentexample.fragments.SignUpFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showSignIn();
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    public void addFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit();
+    }
+
+    public void showSignUp() {
+        replaceFragment(new SignUpFragment());
+    }
+
+    public void showSignIn() {
+        replaceFragment(new SignInFragment());
+    }
+
+    public void showForgotPassword() {
+        replaceFragment(new ForgotPasswordFragment());
+    }
+
+    public void showAbout() {
+        replaceFragment(new AboutFragment());
+    }
+
+    public void signIn(String login, String password) {
+        Toast.makeText(this, "You are logged in!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void signUp(String login, String email, String password) {
+        Toast.makeText(this, "You are Signed up!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void sendRecoverCode(String email) {
+        Toast.makeText(this, "You will receive recover code!", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/teslyuk/androidtutorial/fragmentexample/fragments/SignInFragment.java
@@ -0,0 +1,98 @@
+package com.teslyuk.androidtutorial.fragmentexample.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.teslyuk.androidtutorial.fragmentexample.MainActivity;
+import com.teslyuk.androidtutorial.fragmentexample.R;
+
+/**
+ * Created by taras.teslyuk on 3/23/17.
+ */
+
+public class SignInFragment extends Fragment {
+
+    private View root;
+    private Button signInBtn;
+    private TextView signUpTv, forgotPasswordTv, aboutTv;
+    private EditText loginEt, passwordEt;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_signin, container, false);
+
+        initView();
+        initListeners();
+
+        return root;
+    }
+
+
+    private void initView() {
+        signInBtn = (Button) root.findViewById(R.id.signin_btn);
+        signUpTv = (TextView) root.findViewById(R.id.signup_txt);
+        forgotPasswordTv = (TextView) root.findViewById(R.id.forgot_password_txt);
+        aboutTv = (TextView) root.findViewById(R.id.about_txt);
+        loginEt = (EditText) root.findViewById(R.id.login_et);
+        passwordEt = (EditText) root.findViewById(R.id.password_et);
+    }
+
+    private void initListeners() {
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateInput();
+            }
+        });
+
+        signUpTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).showSignUp();
+            }
+        });
+
+        forgotPasswordTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).showForgotPassword();
+            }
+        });
+
+        aboutTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).showAbout();
+            }
+        });
+    }
+
+    private void validateInput() {
+        loginEt.setError(null);
+        passwordEt.setError(null);
+        String login = loginEt.getText().toString();
+        String password = passwordEt.getText().toString();
+        if (TextUtils.isEmpty(login)) {
+            Toast.makeText(getActivity(), "Please enter login!", Toast.LENGTH_SHORT).show();
+            loginEt.setError("Please enter login!");
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getActivity(), "Please enter password!", Toast.LENGTH_SHORT).show();
+            passwordEt.setError("Please enter password!");
+            return;
+        }
+
+        ((MainActivity) getActivity()).signIn(login, password);
+    }
+
+}
