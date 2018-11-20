573a8098184d41eba08c3fc5d5126ecaba6ef6fc
TeslyukTaras <taras.teslyuk@gmail.com>
2018-11-20 18:44:17 +0200

    add: rewrite signIn to pre MVP;

--- app/src/main/java/com/teslyuk/androidtutorial/fragmentexample/fragments/signin/SignInFragment.java
+++ app/src/main/java/com/teslyuk/androidtutorial/fragmentexample/fragments/signin/SignInFragment.java
@@ -21,78 +21,27 @@ import com.teslyuk.androidtutorial.fragmentexample.R;
 public class SignInFragment extends Fragment {
 
     private View root;
-    private Button signInBtn;
-    private TextView signUpTv, forgotPasswordTv, aboutTv;
-    private EditText loginEt, passwordEt;
 
+    private SignInContract.ISignInView view;
+    private SignInContract.ISignInPresenter presenter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_signin, container, false);
 
-        initView();
-        initListeners();
+        initView(root);
+        initPresenter();
 
         return root;
     }
 
-
-    private void initView() {
-        signInBtn = (Button) root.findViewById(R.id.signin_btn);
-        signUpTv = (TextView) root.findViewById(R.id.signup_txt);
-        forgotPasswordTv = (TextView) root.findViewById(R.id.forgot_password_txt);
-        aboutTv = (TextView) root.findViewById(R.id.about_txt);
-        loginEt = (EditText) root.findViewById(R.id.login_et);
-        passwordEt = (EditText) root.findViewById(R.id.password_et);
+    private void initView(View root) {
+        view = new SignInView();
+        view.init(root);
     }
 
-    private void initListeners() {
-        signInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                validateInput();
-            }
-        });
-
-        signUpTv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ((MainActivity) getActivity()).showSignUp();
-            }
-        });
-
-        forgotPasswordTv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ((MainActivity) getActivity()).showForgotPassword();
-            }
-        });
-
-        aboutTv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ((MainActivity) getActivity()).showAbout();
-            }
-        });
-    }
-
-    private void validateInput() {
-        loginEt.setError(null);
-        passwordEt.setError(null);
-        String login = loginEt.getText().toString();
-        String password = passwordEt.getText().toString();
-        if (TextUtils.isEmpty(login)) {
-            Toast.makeText(getActivity(), "Please enter login!", Toast.LENGTH_SHORT).show();
-            loginEt.setError("Please enter login!");
-            return;
-        }
-
-        if (TextUtils.isEmpty(password)) {
-            Toast.makeText(getActivity(), "Please enter password!", Toast.LENGTH_SHORT).show();
-            passwordEt.setError("Please enter password!");
-            return;
-        }
-
-        ((MainActivity) getActivity()).signIn(login, password);
+    private void initPresenter() {
+        presenter = new SignInPresenter(view, this);
+        view.setPresenter(presenter);
     }
 
 }
